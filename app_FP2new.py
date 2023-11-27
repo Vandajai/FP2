b649e5007d8cf3d916d92ec5e1147d2782178b9a
Vandana Jaiswal <Vandana.jaiswal85@gmail.com>
2023-11-27 22:28:18 +0530

    Added

--- app_FP2new.py
+++ app_FP2new.py
@@ -9,14 +9,15 @@ import os
 
 # Function to get the absolute path based on the script's location
 def get_absolute_path(relative_path):
-    return os.path.join("/home/ec2-user/FP2/Image", relative_path)
+    image_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Image")
+    return os.path.join(image_directory, relative_path)
 
 
 
 st.title('Oil Production Forecast')
 
 # Display an image related to oil production forecast
-oil_production_img_path = get_absolute_path("oil_production_image.png")
+oil_production_img_path = get_absolute_path("oil_production_image.PNG")
 print("Absolute path:", oil_production_img_path)
 oil_production_img = Image.open(oil_production_img_path)
 st.image(oil_production_img, use_column_width=True)
@@ -36,22 +37,22 @@ show_models_results = st.button('Show Models Results')
 if show_models_results:
     if energy_product == 'Crude oil':
         # 2nd Image: GluonTS Simple Feed Forward Model on validation data
-        img_simple_feedforward = Image.open(get_absolute_path("GluonTS Simple Feed Forward Model on Validation Data.png"))
+        img_simple_feedforward = Image.open(get_absolute_path("GluonTS Simple Feed Forward Model on Validation Data.PNG"))
         st.header("GluonTS Simple Feed Forward Model on Validation Data")
         st.image(img_simple_feedforward, use_column_width=True)
 
         # 3rd Image: GluonTS DeepAR Model on Validation Data
-        img_deepar = Image.open(get_absolute_path("GluonTS DeepAR Model on Validation Data.png"))
+        img_deepar = Image.open(get_absolute_path("GluonTS DeepAR Model on Validation Data.PNG"))
         st.header("GluonTS DeepAR Model on Validation Data")
         st.image(img_deepar, use_column_width=True)
 
         # 4th Image: Comparison GluonTS Simple FeedForward Model and Deep AR Model Forecasting on the validation Dataset
-        img_comparison = Image.open(get_absolute_path("Comparison GluonTS Simple FeedForward Model and Deep AR Model Forecasting on Validation Dataset.png"))
+        img_comparison = Image.open(get_absolute_path("Comparison GluonTS Simple FeedForward Model and Deep AR Model Forecasting on Validation Dataset.PNG"))
         st.header("Comparison GluonTS Simple FeedForward Model and Deep AR Model Forecasting on Validation Dataset")
         st.image(img_comparison, use_column_width=True)
 
         # 5th Image: Validation Metrics of simple FFE and DeepAR Model
-        img_metrics = Image.open(get_absolute_path("Validation Metrics of Simple FeedForward and DeepAR Model.png"))
+        img_metrics = Image.open(get_absolute_path("Validation Metrics of Simple FeedForward and DeepAR Model.PNG"))
         st.header("Validation Metrics of Simple FeedForward and DeepAR Model")
         st.image(img_metrics, use_column_width=True)
 
@@ -71,7 +72,7 @@ if show_models_results:
             st.warning("Metrics results file not found.")
 
         # 7th Image: LSTM model
-        img_forecast = Image.open(get_absolute_path("LSTM.png"))
+        img_forecast = Image.open(get_absolute_path("LSTM.PNG"))
         st.header("LSTM Model Results")
         st.image(img_forecast, use_column_width=True)
 
@@ -105,12 +106,12 @@ elif energy_product == 'NGL':
             st.warning("Metrics results file not found.")
 
         # 2nd Image: GluonTS Simple Feed Forward Model on validation data
-        img_simple_feedforward = Image.open(get_absolute_path("Arima.png"))
+        img_simple_feedforward = Image.open(get_absolute_path("Arima.PNG"))
         st.header("ARIMA & SARIMA Model Forecasting")
         st.image(img_simple_feedforward, use_column_width=True)
 
         # 3rd Image: GluonTS Simple Feed Forward Model on validation data
-        img_simple_feedforward = Image.open(get_absolute_path("Sarima.png"))
+        img_simple_feedforward = Image.open(get_absolute_path("Sarima.PNG"))
         st.header("SARIMA Model Forecasting")
         st.image(img_simple_feedforward, use_column_width=True)
 
@@ -118,7 +119,7 @@ elif energy_product == 'NGL':
 elif energy_product == 'Other':
         
   
-        img_Other = Image.open(get_absolute_path("US_others.png"))
+        img_Other = Image.open(get_absolute_path("US_others.PNG"))
         st.header("Prophet Model Results for Others")
         st.image(img_Other, use_column_width=True)
 
